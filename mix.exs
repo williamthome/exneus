675a207ad622b50e844e791c6cab6933cdf83c13
William Fank Thomé <willegp88@gmail.com>
2024-08-07 13:34:41 -0300

    Install ex_doc

--- mix.exs
+++ mix.exs
@@ -23,7 +23,8 @@ defmodule Exneus.MixProject do
     [
       # {:euneus, "~> 2.1"},
       {:euneus, git: "https://github.com/williamthome/euneus", branch: "main"},
-      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false}
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.21", only: :dev, runtime: false}
     ]
   end
 end
