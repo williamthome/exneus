1c982eda2b820a9c88adb8b92321d2fe347112dc
William Fank Thomé <willegp88@gmail.com>
2024-08-08 21:31:25 -0300

    Add more OTP and Elixir versions to CI

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Exneus.MixProject do
     [
       app: :exneus,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       preferred_cli_env: [
